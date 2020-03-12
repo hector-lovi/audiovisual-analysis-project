a4739bd90173ac8502a1c06725017e1203d6430c
Mariló Molina Candela <hec.lopezvizcaino@gmail.com>
2020-03-12 12:44:23 +0100

    api flask

---
+++ src/flask_api.py
@@ -0,0 +1,14 @@
+from flask import Flask, flash, request, send_from_directory, render_template, json, redirect, url_for, session
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__, static_folder='./templates/')
+
+
+@app.route('/')
+def showmain():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
